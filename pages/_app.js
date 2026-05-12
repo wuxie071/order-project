3e1d71435168d8693788070be690a72c1c1121ed
wuxie071 <1013754456@qq.com>
2026-05-12 11:46:56 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.css' // 必须有两个点，表示跳出 pages 文件夹找到 styles
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
